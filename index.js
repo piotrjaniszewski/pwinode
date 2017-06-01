feee19ccaff05f6499bcb817c3316e478dd9a9e3
Piotr Janiszewski <p.janisz2@gmail.com>
2017-06-02 00:28:58 +0200

    sequelize

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var port = process.env.PORT || 1337;
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
+//
+// app.get('/', function (req, res) {
+//     res.send('Hello World!')
+// })
+//
+// app.listen(port, function () {
+//     console.log('Example app listening on port 1337!')
+// })
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+var express = require('express')
+var cors = require('cors')
+var Sequelize = require('sequelize')
+var bodyParser = require('body-parser')
+var routes = require('./routes.js')
 
-app.listen(port, function () {
-    console.log('Example app listening on port 1337!')
-})
+var app = express()
+app.use(cors())
+app.use(bodyParser.json())
+routes(app)
+
+app.listen(port)
+console.log('Server started at port 3000 ...')
