fdf8acdc92a853f7ab34c4b9b2bf24aef98e52d8
Piotr Janiszewski <p.janisz2@gmail.com>
2017-06-02 00:39:07 +0200

    sequelize fix final

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+module.exports = function(app) {
+    app.get('/', function (req, res) {
+        res.send('Hello World!')
+    })
+};
