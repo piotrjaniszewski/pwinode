6c6e90a69db12192256ce168648271fc115ed8a6
Piotr Janiszewski <p.janisz2@gmail.com>
2017-06-02 00:23:03 +0200

    portfix

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-// var http = require('http');
-//
-// var server = http.createServer(function(request, response) {
-//
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello World!");
-//
-// });
-//
-// var port = process.env.PORT || 1337;
-// server.listen(port);
-//
-// console.log("Server running at http://localhost:%d", port);
+var port = process.env.PORT || 1337;
 const express = require('express')
 const app = express()
 
@@ -18,6 +6,6 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
-app.listen(1337, function () {
+app.listen(port, function () {
     console.log('Example app listening on port 1337!')
 })
