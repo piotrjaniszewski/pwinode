9f63fe7d131ad66ea21ad6a991a685ec6e09a4a5
Piotr Janiszewski <p.janisz2@gmail.com>
2017-06-02 00:17:22 +0200

    express

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var http = require('http');
+// var http = require('http');
+//
+// var server = http.createServer(function(request, response) {
+//
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World!");
+//
+// });
+//
+// var port = process.env.PORT || 1337;
+// server.listen(port);
+//
+// console.log("Server running at http://localhost:%d", port);
+const express = require('express')
+const app = express()
 
-var server = http.createServer(function(request, response) {
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
-});
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(1337, function () {
+    console.log('Example app listening on port 1337!')
+})
